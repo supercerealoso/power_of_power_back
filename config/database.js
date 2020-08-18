ff1775e6afafb9b7bb07ba72b4e4416b88e242dc
operezcham <operezcham@gmail.com>
2020-08-18 03:00:30 -0500

    database

--- config/database.js
+++ config/database.js
@@ -70,12 +70,13 @@ module.exports = {
   */
   pg: {
     client: 'pg',
-    connection: {
+    /*connection: {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
       database: Env.get('DB_DATABASE', 'adonis')
-    }
+    }*/
+    connection: Env.get('DATABASE_URL', '')
   }
 }
