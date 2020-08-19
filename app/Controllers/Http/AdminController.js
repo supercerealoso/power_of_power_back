cd91af59b898635cd46bbbf2a8f9493fdc386032
operezcham <operezcham@gmail.com>
2020-08-18 19:52:11 -0500

    admin

--- app/Controllers/Http/AdminController.js
+++ app/Controllers/Http/AdminController.js
@@ -6,6 +6,8 @@ const Env = use('Env');
 class AdminController {
     async create() {
         const user = await User.findOrCreate({
+            username: Env.getOrFail('ADMIN_USER')
+        }, {
             username: Env.getOrFail('ADMIN_USER'),
             password: Env.getOrFail('ADMIN_PASS'),
             email: Env.getOrFail('ADMIN_EMAIL')
