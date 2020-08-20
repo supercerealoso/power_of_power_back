0cd2b8c231eef922aef7c058b9cf7ddecd42cbf1
operezcham <operezcham@gmail.com>
2020-08-20 10:10:45 -0500

    explicit dbname

--- app/Controllers/Http/ComicController.js
+++ app/Controllers/Http/ComicController.js
@@ -49,8 +49,8 @@ class ComicController {
         const mongo = new MongoClient(Env.get('MONGODB_URL', ''), {
             useNewUrlParser: true
         });
-        const db = await mongo.connect();
-        const collection = db.collection('comics');
+        await mongo.connect();
+        const collection = mongo.db('powerofpower').collection('comics');
         const comic = await collection.find({
             index: +request.input('index')
         }).next();
