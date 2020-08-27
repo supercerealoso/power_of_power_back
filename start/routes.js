89fe2fd533bda16e4dd088bb90989329eb094d17
operezcham <operezcham@gmail.com>
2020-08-27 12:55:31 -0500

    post create

--- start/routes.js
+++ start/routes.js
@@ -31,3 +31,6 @@ Route.post('/comic/publish', 'ComicController.publish');
 Route.post('/comic/count', 'ComicController.count');
 Route.post('/comic/archive', 'ComicController.archive');
 Route.post('/comic/index', 'ComicController.index');
+
+Route.on('/post/create').render('post.create');
+Route.post('/post/create', 'PostController.post');
